ba1d59571531922693931caf33a7c99e90d472b5
Roza Keshishian <kesishroza@gmail.com>
2025-07-04 13:18:27 +0200

    Lexer realization

--- nelox/scanner.py
+++ nelox/scanner.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from .nelox_token import Token
-from .tokenType import TokenType
+from .token_type import TokenType
 
 
 def is_alpha_numeric(c):
@@ -17,20 +17,6 @@ def is_digit(c):
 
 
 class Scanner:
-    keywords: dict[str, TokenType] = {
-        "and": TokenType.AND,
-        "false": TokenType.FALSE,
-        "func": TokenType.FUNC,
-        "for": TokenType.FOR,
-        "if": TokenType.IF,
-        "list": TokenType.LIST,
-        "or": TokenType.OR,
-        "print": TokenType.PRINT,
-        "return": TokenType.RETURN,
-        "set": TokenType.SET,
-        "true": TokenType.TRUE,
-        "while": TokenType.WHILE
-    }
 
     def __init__(self, source: str):
         self.source: str = source
@@ -42,7 +28,9 @@ class Scanner:
     def scan_tokens(self) -> List[Token]:
         while not self.is_at_end():
             self.start = self.current
-            self.scan_token()
+            token = self.next_token()
+            if token:
+                self.tokens.append(token)
 
         self.tokens.append(Token(TokenType.EOF, "", None, self.line))
         return self.tokens
@@ -55,25 +43,28 @@ class Scanner:
         self.current += 1
         return c
 
-    def add_token(self, type: TokenType, literal=None):
+    def add_token(self, type: TokenType, literal=None) -> Token:
         text = self.source[self.start:self.current]
-        self.tokens.append(Token(type, text, literal, self.line))
+        return Token(type, text, literal, self.line)
 
-    def scan_token(self):
+    def next_token(self) -> Token | None:
         c = self.advance()
 
         if c == '(':
-            self.add_token(TokenType.LEFT_PAREN)
+            return self.add_token(TokenType.LEFT_PAREN)
         elif c == ')':
-            self.add_token(TokenType.RIGHT_PAREN)
+            return self.add_token(TokenType.RIGHT_PAREN)
         elif c.isdigit():
-            self.number()
+            return self.number()
         elif is_alpha(c):
-            self.identifier()
+            return self.identifier()
         elif c == '"':
-            self.string()
-        elif c in (' ', '\r', '\t', '\n'):
-            pass
+            return self.string()
+        elif c in (' ', '\r', '\t'):
+            return None
+        elif c == '\n':
+            self.line += 1
+            return None
         else:
             raise SyntaxError(f"[line {self.line}] Unexpected character: '{c}'")
 
@@ -95,7 +86,7 @@ class Scanner:
             return '\0'
         return self.source[self.current + 1]
 
-    def string(self):
+    def string(self) -> Token:
         while self.peek() != '"' and not self.is_at_end():
             if self.peek() == '\n':
                 self.line += 1
@@ -105,26 +96,22 @@ class Scanner:
             raise SyntaxError(f"[line {self.line}] Unterminated string.")
 
         self.advance()
-
         value = self.source[self.start + 1: self.current - 1]
-        self.add_token(TokenType.STRING, value)
+        return self.add_token(TokenType.STRING, value)
 
-    def number(self):
+    def number(self) -> Token:
         while self.peek().isdigit():
             self.advance()
-        if self.peek() == '.' and self.peek_next().isdigit():
-            self.advance()
-
-            while self.peek().isdigit():
-                self.advance()
 
         text = self.source[self.start:self.current]
-        self.add_token(TokenType.NUMBER, float(text))
+        return self.add_token(TokenType.NUMBER, int(text))
 
-    def identifier(self):
+    def identifier(self) -> Token:
         while is_alpha_numeric(self.peek()):
             self.advance()
 
-        text = self.source[self.start:self.current]
-        token_type = self.keywords.get(text, TokenType.IDENTIFIER)
-        self.add_token(token_type)
+        return self.add_token(TokenType.IDENTIFIER)
+
+
+def scan_all_tokens(source: str) -> List[Token]:
+    return Scanner(source).scan_tokens()
